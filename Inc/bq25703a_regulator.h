55c3de04987eb01b2178144b28477ee59d8a5020
AlexKlimaj <alex@arkelectron.com>
2019-02-19 17:09:05 -0700

    Basic 4S Charging Working

--- Inc/bq25703a_regulator.h
+++ Inc/bq25703a_regulator.h
@@ -27,8 +27,14 @@ extern "C" {
 
 #define MANUFACTURER_ID_ADDR		0x2E
 #define DEVICE_ID_ADDR				0x2F
-#define MAX_CHARGE_VOLTAGE_ADDR_1	0x05
-#define MAX_CHARGE_VOLTAGE_ADDR_2	0x04
+#define MAX_CHARGE_VOLTAGE_ADDR		0x04
+#define CHARGE_CURRENT_ADDR			0x02
+#define CHARGE_OPTION_0_ADDR_1		0x01
+#define CHARGE_OPTION_0_ADDR_2		0x00
+#define MINIMUM_SYSTEM_VOLTAGE_ADDR	0x0D
+
+#define EN_LWPWR					0b0
+#define EN_OOA						0b1
 
 uint8_t Get_Regulator_Connection_State(void);
 void vRegulator(void const *pvParameters);
--- Src/bq25703a_regulator.c
+++ Src/bq25703a_regulator.c
@@ -11,6 +11,7 @@
 #include "error.h"
 #include "main.h"
 #include "string.h"
+#include "printf.h"
 
 extern I2C_HandleTypeDef hi2c1;
 
@@ -39,8 +40,10 @@ uint8_t Query_Regulator_Connection(void);
 uint8_t Read_Charge_Okay(void);
 void Regulator_HI_Z(uint8_t hi_z_en);
 void Regulator_OTG_EN(uint8_t otg_en);
+void Regulator_Set_Charge_Option_0(void);
 void Set_Charge_Voltage(uint8_t number_of_cells);
 void I2C_Write_Register(uint8_t addr_to_write, uint8_t *pData);
+void I2C_Write_Two_Byte_Register(uint8_t addr_to_write, uint8_t lsb_data, uint8_t msb_data);
 void I2C_Read_Register(uint8_t addr_to_read, uint8_t *pData);
 
 /**
@@ -141,10 +144,10 @@ uint8_t Read_Charge_Okay() {
 
 /**
  * @brief Enables or disables high impedance mode on the output of the regulator
- * @param hi_z_en 0 puts the output of the regulator in hiz mode. 1 takes the regulator out of hi_z and allows charging
+ * @param hi_z_en 1 puts the output of the regulator in hiz mode. 0 takes the regulator out of hi_z and allows charging
  */
 void Regulator_HI_Z(uint8_t hi_z_en) {
-	if (hi_z_en == 0) {
+	if (hi_z_en == 1) {
 		HAL_GPIO_WritePin(ILIM_HIZ_GPIO_Port, ILIM_HIZ_Pin, GPIO_PIN_RESET);
 	}
 	else {
@@ -166,6 +169,39 @@ void Regulator_OTG_EN(uint8_t otg_en) {
 }
 
 /**
+ * @brief Sets Charge Option 0 Based on #defines in header
+ */
+void Regulator_Set_Charge_Option_0() {
+
+	uint8_t charge_option_0_register_1_value = 0b01100110;
+	uint8_t charge_option_0_register_2_value = 0b00001110;
+
+	I2C_Write_Register(CHARGE_OPTION_0_ADDR_1, (uint8_t *) &charge_option_0_register_1_value);
+	I2C_Write_Register(CHARGE_OPTION_0_ADDR_2, (uint8_t *) &charge_option_0_register_2_value);
+
+	return;
+}
+
+/**
+ * @brief Sets the charging current limit. From 64mA to 8.128A in 64mA steps. Maps from 0 - 128. 7 bit value.
+ * @param charge_current_limit 0-128 which remaps from 64mA-8.128A. 7 bit value.
+ */
+void Set_Charge_Current(uint8_t charge_current_limit) {
+
+	uint8_t charge_current_register_1_value = 0;
+	uint8_t charge_current_register_2_value = 0;
+
+	if ((charge_current_limit >= 0) || (charge_current_limit <= 128)) {
+		charge_current_register_1_value = (charge_current_limit >> 3);
+		charge_current_register_2_value = (charge_current_limit << 6);
+	}
+
+	I2C_Write_Two_Byte_Register(CHARGE_CURRENT_ADDR, charge_current_register_2_value, charge_current_register_1_value);
+
+	return;
+}
+
+/**
  * @brief Sets the charging voltage based on the number of cells. 1 - 4.192V, 2 - 8.400V, 3 - 12.592V, 4 - 16.800V
  * @param number_of_cells number of cells connected
  */
@@ -174,35 +210,43 @@ void Set_Charge_Voltage(uint8_t number_of_cells) {
 	uint8_t max_charge_register_1_value = 0;
 	uint8_t max_charge_register_2_value = 0;
 
-	if ((number_of_cells > 4) || (number_of_cells < 1)) {
-		max_charge_register_1_value = 0;
-		max_charge_register_2_value = 0;
-		I2C_Write_Register(MAX_CHARGE_VOLTAGE_ADDR_1, (uint8_t *) &max_charge_register_1_value);
-		I2C_Write_Register(MAX_CHARGE_VOLTAGE_ADDR_2, (uint8_t *) &max_charge_register_2_value);
-		return;
+	uint8_t	minimum_system_voltage_value = 0b00000100; //1.024V
+
+	if ((number_of_cells > 0) || (number_of_cells < 5)) {
+		switch (number_of_cells) {
+			case 1:
+				max_charge_register_1_value = 0b00010000;
+				max_charge_register_2_value = 0b01100000;
+				minimum_system_voltage_value = 0b00001011; //2.816V
+				break;
+			case 2:
+				max_charge_register_1_value = 0b00100000;
+				max_charge_register_2_value = 0b11010000;
+				minimum_system_voltage_value = 0b00010110; //5.632V
+				break;
+			case 3:
+				max_charge_register_1_value = 0b00110001;
+				max_charge_register_2_value = 0b00110000;
+				minimum_system_voltage_value = 0b00100001; //8.448V
+				break;
+			case 4:
+				max_charge_register_1_value = 0b01000001;
+				max_charge_register_2_value = 0b10100000;
+				minimum_system_voltage_value = 0b00101100; //11.264V
+				break;
+			default:
+				max_charge_register_1_value = 0;
+				max_charge_register_2_value = 0;
+				minimum_system_voltage_value = 0b00000100; //1.024V
+				break;
+			}
 	}
 
-	switch (number_of_cells) {
-	case 1:
-		max_charge_register_1_value = 0b00010000;
-		max_charge_register_2_value = 0b01100000;
-		break;
-	case 2:
-		max_charge_register_1_value = 0b00100000;
-		max_charge_register_2_value = 0b11010000;
-		break;
-	case 3:
-		max_charge_register_1_value = 0b00110001;
-		max_charge_register_2_value = 0b00110000;
-		break;
-	case 4:
-		max_charge_register_1_value = 0b01000001;
-		max_charge_register_2_value = 0b10100000;
-		break;
-	}
+	I2C_Write_Register(MINIMUM_SYSTEM_VOLTAGE_ADDR, (uint8_t *) &minimum_system_voltage_value);
 
-	I2C_Write_Register(MAX_CHARGE_VOLTAGE_ADDR_1, (uint8_t *) &max_charge_register_1_value);
-	I2C_Write_Register(MAX_CHARGE_VOLTAGE_ADDR_2, (uint8_t *) &max_charge_register_2_value);
+	I2C_Write_Two_Byte_Register(MAX_CHARGE_VOLTAGE_ADDR, max_charge_register_2_value, max_charge_register_1_value);
+
+	return;
 }
 
 /**
@@ -212,6 +256,40 @@ void Set_Charge_Voltage(uint8_t number_of_cells) {
 void I2C_Write_Register(uint8_t addr_to_write, uint8_t *pData) {
 		I2C_Transfer((uint8_t *)&addr_to_write, 1);
 		I2C_Transfer(pData, 1);
+}
+
+/**
+ * @brief Automatically performs three I2C writes to write a two byte register on the regulator
+ * @param lsb_data Pointer to least significant byte of data to be transferred
+ * @param msb_data Pointer to most significant byte of data to be transferred
+ */
+void I2C_Write_Two_Byte_Register(uint8_t addr_to_write, uint8_t lsb_data, uint8_t msb_data) {
+
+		uint8_t data[3];
+		data[0] = addr_to_write;
+		data[1] = lsb_data;
+		data[2] = msb_data;
+
+		if ( xSemaphoreTake( xTxMutex_Regulator, cmdMAX_MUTEX_WAIT ) == pdPASS) {
+			do
+			{
+				TickType_t xtimeout_start = xTaskGetTickCount();
+				while (HAL_I2C_Master_Transmit_DMA(&hi2c1, (uint16_t)BQ26703A_I2C_ADDRESS, (uint8_t *)data, 3) != HAL_OK) {
+					if (((xTaskGetTickCount()-xtimeout_start)/portTICK_PERIOD_MS) > I2C_TIMEOUT) {
+						Set_Error_State(REGULATOR_COMMUNICATION_ERROR);
+						break;
+					}
+				}
+			    while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY) {
+					if (((xTaskGetTickCount()-xtimeout_start)/portTICK_PERIOD_MS) > I2C_TIMEOUT) {
+						Set_Error_State(REGULATOR_COMMUNICATION_ERROR);
+						break;
+					}
+			    }
+			}
+			while(HAL_I2C_GetError(&hi2c1) == HAL_I2C_ERROR_AF);
+			xSemaphoreGive(xTxMutex_Regulator);
+		}
 }
 
 /**
@@ -231,7 +309,7 @@ void vRegulator(void const *pvParameters) {
 	TickType_t xDelay = 500 / portTICK_PERIOD_MS;
 
 	/* Disable the output of the regulator for safety */
-	Regulator_HI_Z(0);
+	Regulator_HI_Z(1);
 
 	/* Disable OTG mode */
 	Regulator_OTG_EN(0);
@@ -239,8 +317,12 @@ void vRegulator(void const *pvParameters) {
 	/* Check if the regulator is connected */
 	regulator.connected = Query_Regulator_Connection();
 
-	//Set_Charge_Voltage(1);
-	//Regulator_HI_Z(1);
+	/* Set Charge Option 0 */
+	Regulator_Set_Charge_Option_0();
+
+	Set_Charge_Voltage(4);
+	Set_Charge_Current(5);
+	Regulator_HI_Z(0);
 
 	for (;;) {
 
