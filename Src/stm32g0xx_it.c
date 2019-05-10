1eba0f99db242c0c7c530dfacd9f836beda92bb3
AlexKlimaj <alex@arkelectron.com>
2019-05-09 18:12:37 -0600

    USB PD Charging MVP (#16)
    Basic voltage selection and setting charge current based on available power from supply finished.

--- Src/stm32g0xx_it.c
+++ Src/stm32g0xx_it.c
@@ -233,7 +233,7 @@ void TIM2_IRQHandler(void)
   /* USER CODE BEGIN TIM2_IRQn 0 */
 
   /* USER CODE END TIM2_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim2);
+  //HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
 
   /* USER CODE END TIM2_IRQn 1 */
