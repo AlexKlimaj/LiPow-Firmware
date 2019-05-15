0a9c92c22c4e86517cf5ca5566326d630085a4b2
AlexKlimaj <alex@arkelectron.com>
2019-05-15 17:20:49 -0600

    Update ADC Scales for Rev 3

--- Inc/adc_interface.h
+++ Inc/adc_interface.h
@@ -17,30 +17,31 @@ extern "C" {
 #include "cmsis_os.h"
 
 // Calculate the voltages based on the following data:
-// https://docs.google.com/spreadsheets/d/1uXEK7AOaoLDN3ATuCtUCRYjI7-zBhgmvHDWbn-7-k90/edit?usp=sharing
+// Rev 2.0 https://docs.google.com/spreadsheets/d/1uXEK7AOaoLDN3ATuCtUCRYjI7-zBhgmvHDWbn-7-k90/edit?usp=sharing
+// Rev 3.0 https://docs.google.com/spreadsheets/d/18t-933Qyvu73WHUnGbybxskIo2osHvycsP7GtKGH-D8/edit?usp=sharing
 
 #define ADC_FILTER_SUM_COUNT		380
 
 #define BATTERY_ADC_MULTIPLIER 		100000
 
 #define BATTERY_ADC_OFFSET 			(int32_t)( 0 )
-#define BATTERY_ADC_SCALAR 			(uint32_t)( 421 )
+#define BATTERY_ADC_SCALAR 			(uint32_t)( 449 )
 #define BATTERY_MIN_ADC_READING 	20
 
 #define CELL_ONE_ADC_OFFSET 		(int32_t)( 0 )
-#define CELL_ONE_ADC_SCALAR 		(uint32_t)( 111 )
+#define CELL_ONE_ADC_SCALAR 		(uint32_t)( 112 )
 #define CELL_ONE_MIN_ADC_READING 	20
 
 #define CELL_TWO_ADC_OFFSET 		(int32_t)( 0 )
-#define CELL_TWO_ADC_SCALAR 		(uint32_t)( 213 )
+#define CELL_TWO_ADC_SCALAR 		(uint32_t)( 215 )
 #define CELL_TWO_MIN_ADC_READING 	20
 
 #define CELL_THREE_ADC_OFFSET 		(int32_t)( 0 )
-#define CELL_THREE_ADC_SCALAR 		(uint32_t)( 323 )
+#define CELL_THREE_ADC_SCALAR 		(uint32_t)( 327 )
 #define CELL_THREE_MIN_ADC_READING 	20
 
 #define CELL_FOUR_ADC_OFFSET 		(int32_t)( 0 )
-#define CELL_FOUR_ADC_SCALAR 		(uint32_t)( 421 )
+#define CELL_FOUR_ADC_SCALAR 		(uint32_t)( 449 )
 #define CELL_FOUR_MIN_ADC_READING 	20
 
 #define CELL_MAX_VOLTAGE			(uint32_t)( 5 * BATTERY_ADC_MULTIPLIER )
